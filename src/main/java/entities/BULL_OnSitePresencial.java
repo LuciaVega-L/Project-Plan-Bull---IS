21613ced29e4f30c68f07d43dc233a7f3030c0ca
Lucia Vega <lu.al.vega@unillanos.edu.co>
2026-05-28 23:05:53 -0500

    refactoring of entity classes
    Some changes were made to the implementation of the modality entity with its derivatives and group for greater consistency with the diagrams established in previous phases of the project

--- src/main/java/entities/BULL_OnSitePresencial.java
+++ src/main/java/entities/BULL_OnSitePresencial.java
@@ -2,20 +2,15 @@ package entities;
 
 public class BULL_OnSitePresencial extends BULL_Modality {
 
-    private BULL_Ubication ubication;
-
-    public BULL_OnSitePresencial(BULL_Ubication ubication) {
-        super("Presencial");
-        if (ubication == null) {
-            throw new IllegalArgumentException("La ubicación para modalidad presencial no puede ser nula.");
-        }
-        this.ubication = ubication;
+    public BULL_OnSitePresencial() {
+        super();
     }
 
-    public BULL_Ubication getUbication() { return ubication; }
+    @Override
+    public String getMode() { return "Presencial"; }
 
     @Override
     public String toString() {
-        return "OnSitePresencial{ubication=" + ubication + ", groups=" + getGroups().size() + "}";
+        return "OnSitePresencial{groups=" + getGroups().size() + "}";
     }
 }
